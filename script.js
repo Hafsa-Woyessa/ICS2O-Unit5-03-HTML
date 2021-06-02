3adf84216536b367ad64e46f6ddef8ae508605be
HafsaWoyessa <>
2021-06-02 13:06:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('report').addEventListener
+('click',reportResults)
+
+function displayMessage () {
+  document.getElementById('paragraph').innerHTML =
+  'I love food'
+}
+
+function reportResults () { 
+  alert(document.getElementById('input').value)
+}
+
+// This line displays a message
+alert('dear haiti')
+
+alert('love alaine') // This line displays a  message
