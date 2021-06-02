560f5b72c7721a45cbd421298d3c93c275b99f34
HafsaWoyessa <>
2021-06-02 13:29:56 +0000

    line 2

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-document.getElementById('report').addEventListener('click', 
-reportResults)
+document.getElementById('report').addEventListener('click',
+  reportResults)
 
 function reportResults () {
   alert(document.getElementById('input').value)
 }
 
-function displayMessage () {
-  document.getElementById('paragraph').innerHTML =
-  'I love food'
-}
 // This line displays a message
 alert('The computer displays an alert')
 
